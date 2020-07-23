0e1427c528f8e607026f093b4d5e3793be72b75c
Caleb Trepowski <calebtrepowski@gmail.com>
2020-07-23 14:12:01 -0400

    Tarea 1 para entregar

--- MN/Tarea1/Ejercicio2.py
+++ MN/Tarea1/Ejercicio2.py
@@ -1,4 +1,14 @@
-'''Resuelva la ecuacion pow(x,x)=100 a partir de una iteracion
+'''
+********************************************************
+========== PROGRAMA HECHO EN Python v3.8.1 ============
+----- numpy v1.18.1, matplotlib v3.1.3
+Metodos Numericos, FIUNA, 4to semestre Ing Mecatronica
+Alumno: CALEB TREPOWSKI CASTILLO
+********************************************************
+'''
+
+''' Tarea 1, Ejercicio 2
+Resuelva la ecuacion pow(x,x)=100 a partir de una iteracion
 de punto fijo
 1.Convierta previamente la ecuacion a la forma x = g(x) para
 su resolucion. Muestre su proceso.
@@ -8,9 +18,10 @@ punto fijo sobre la forma obtenida en el item 1.
 import numpy as np
 import matplotlib.pyplot as plt
 
-f = lambda x: pow(x,x) - 100 #funcion de la que queremos
-                             #hallar las raices
-x = np.linspace(0.1,4,150)
+f = lambda x: x - (2/np.log10(x))
+#funcion de la que queremos hallar las raices
+
+x = np.linspace(1.1,4,150)
 y = f(x)
 plt.plot(x,y)
 plt.grid()
@@ -22,8 +33,10 @@ p0 = 3.5
 iter_max = 50
 error = 1e-5
 
-g = lambda x: 2/np.log10(x) #funcion iterante
-                            #hallada manualmente
+g = lambda x: 2/np.log10(x)
+#funcion iterante hallada manualmente de la que queremos
+#hallar sus puntos fijos
+
 p = np.array([p0])
 
 for i in range(0,iter_max):
--- MN/Tarea1/Ejercicio3.m
+++ MN/Tarea1/Ejercicio3.m
@@ -1,3 +1,10 @@
+% ********************************************************
+% ======== PROGRAMA HECHO EN GNU OCTAVE v5.2.0 ==========
+% Metodos Numericos, FIUNA, 4to semestre Ing Mecatronica
+% Alumno: CALEB TREPOWSKI CASTILLO
+% ********************************************************
+
+% Tarea 1, Ejercicio 3
 %Demuestre que la siguiente formula de iteracion nos permite
 %hallar la raiz cubica de un numero real N cualquiera:
 %pk+1 =(2*pk + N/pk^2)/3
@@ -5,20 +12,23 @@
 %raiz cubica de cualquier numero real N con la formula
 %propuesta anteriormente.
 format long
-N = 1; %reemplazar N por el numero del que se desea
-       %obtener la raiz cubica
+% reemplazar N por el numero del que se desea obtener la
+% raiz cubica
+N = 1; 
+
 iter_max = 50;
 err = 1e-5;
 
-p0 = N/3;
+p0 = N/3; %chute inicial considerado para todo N.
 p = [p0];
 
 for i = 1:iter_max
   p(i+1) = (2*p(i) + N/(p(i)^2))/3;
-  if abs(  (p(i+1)-p(i))/p(i+1)  ) < err || abs(  p(i+1)-p(i)  ) < err
+  if abs((p(i+1)-p(i))/p(i+1)) < err || abs(p(i+1)-p(i)) < err
+	%criterios de error absoluto y relativo por separado para
+	%poder trabajar tanto con numeros grandes y numeros chicos
     break
   end
 end
 
-p(length(p))
-i
+p(length(p)) %imprimir el resultado
